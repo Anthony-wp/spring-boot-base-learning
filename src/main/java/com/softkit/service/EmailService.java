2b6af22a7bc856cd310862fa269ed1c7e1211e2f
Anton Ponomarenko <anthone.vallpon@softkit.config>
2021-08-04 16:37:14 +0300

    fix EmailService

--- src/main/java/com/softkit/service/EmailService.java
+++ src/main/java/com/softkit/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.softkit.service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.mail.*;
@@ -7,30 +8,29 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
-import java.io.File;
 import java.util.Properties;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
-//    todo move to properties
-    private final String host = "smtp.gmail.com";
-    private final int port = 587;
-    private final String username = "anthony.vallpon@gmail.com";
-    private final String password = "planck662607015";
+    @Value("${email.service.host}")
+    private final String host;
+    @Value("${email.service.port}")
+    private final int port;
+    @Value("${email.service.username}")
+    private final String username;
+    @Value("${email.service.password}")
+    private final String password;
 
 
     public void sendMail(String recipient, String msg){
-//    todo move to properties
         Properties prop = new Properties();
         prop.put("mail.smtp.auth", true);
         prop.put("mail.smtp.starttls.enable", "true");
         prop.put("mail.smtp.host", host);
         prop.put("mail.smtp.port", port);
         prop.put("mail.smtp.ssl.trust", host);
-
 
         Session session = Session.getInstance(prop, new Authenticator() {
             @Override
@@ -41,13 +41,10 @@ public class EmailService {
 
         try {
 
-
             Message message = new MimeMessage(session);
-            //    todo move to properties
-            message.setFrom(new InternetAddress("anthony.vallpon@gmail.com"));
+            message.setFrom(new InternetAddress(username));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
-            message.setSubject("Registration successful ");
-
+            message.setSubject("Registration successful");
 
             MimeBodyPart mimeBodyPart = new MimeBodyPart();
             mimeBodyPart.setContent(msg, "text/html");
